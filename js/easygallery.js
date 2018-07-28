4d499559cf5c278d3d92be5230206310f0494de2
Ranacode <romerodeveloper@techie.com>
2018-07-28 19:04:23 +0100

    Switch toggle on top navbar for change the background color, like sun-night mode

---
+++ js/easygallery.js
@@ -0,0 +1,18 @@
+const checkbox = document.querySelector("input[type=checkbox");
+
+checkbox.addEventListener("change", toggleSwitch, false);
+
+function toggleSwitch(e) {
+  const checkbox = e.target;
+  const label = checkbox.parentElement;
+  const sunIcon = label.previousElementSibling;
+  const moonIcon = label.nextElementSibling;
+
+  if (checkbox.checked) {
+    sunIcon.style.color = "gray";
+    moonIcon.style.color = "#055199";
+  } else {
+    sunIcon.style.color = "#e4e40c";
+    moonIcon.style.color = "gray";
+  }
+}
