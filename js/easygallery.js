4174edcbd48adee1e5130b18f51a12edd25256b0
Ranacode <romerodeveloper@techie.com>
2018-07-29 10:41:22 +0100

    Create images dinamically and added spinner to improve the UX

--- js/easygallery.js
+++ js/easygallery.js
@@ -1,11 +1,15 @@
+const galleryGrid = document.querySelector(".PhotoGallery__Grid");
 const checkbox = document.querySelector("input[type=checkbox");
-let path = "/mockdata/images.json";
+const thumbnailDisplay = document.querySelector("i.fa-th");
+const largeDisplay = document.querySelector("i.fa-th-large");
+
+let mockdataPath = "/mockdata/images.json";
 
 checkbox.addEventListener("change", toggleSwitch, false);
 
-renderGalleryImages(path);
+renderGalleryImages(mockdataPath);
 
-function renderGalleryImages(path) {
+function renderGalleryImages(path, display = "thumbnail") {
   const { host, protocol } = window.location;
   const url = `${protocol}//${host}/${path}`;
 
@@ -20,8 +24,33 @@ function renderGalleryImages(path) {
     .then(response => response.json())
     .then(data => {
       const images = data;
+      galleryGrid.innerHTML = "";
+      images.forEach(image => {
+        galleryGrid.appendChild(createSpinnerFragment());
+        setTimeout(() => {
+          createImageElement(image);
+        }, 1000);
+      });
     })
     .catch(err => console.log(err));
+}
+
+function createImageElement(image) {
+  const { thumbnail, url, name, place, caption } = image;
+  const fragment = document.createDocumentFragment();
+  let View = document.createElement("div");
+  View.setAttribute("class", "PhotoGallery__View");
+  const imageElement = new Image();
+  imageElement.src = thumbnail;
+  imageElement.alt = place;
+  imageElement.setAttribute("class", "PhotoGallery__Img thumbnail");
+  let Mask = document.createElement("div");
+  Mask.setAttribute("class", "PhotoGallery__Img--mask");
+  View.appendChild(imageElement);
+  View.appendChild(Mask);
+  fragment.appendChild(View);
+  galleryGrid.removeChild(document.querySelector("div.sk-cube-grid"));
+  galleryGrid.appendChild(fragment);
 }
 
 function toggleSwitch(e) {
@@ -34,7 +63,7 @@ function changeBackgroundTheme(label, checked) {
   const sunIcon = label.previousElementSibling;
   const moonIcon = label.nextElementSibling;
 
-  if (checkbox.checked) {
+  if (checked) {
     sunIcon.style.color = "gray";
     moonIcon.style.color = "#fac9b8";
     document.body.style.backgroundColor = "#1e2019";
@@ -43,4 +72,17 @@ function changeBackgroundTheme(label, checked) {
     moonIcon.style.color = "gray";
     document.body.style.backgroundColor = "#fafafa";
   }
+}
+
+function createSpinnerFragment() {
+  const fragment = document.createDocumentFragment();
+  const cube = document.createElement("div");
+  cube.setAttribute("class", "sk-cube-grid");
+  for (let index = 1; index <= 9; index++) {
+    let cubeChild = document.createElement("div");
+    cubeChild.setAttribute("class", `sk-cube sk-cube${index}`);
+    cube.appendChild(cubeChild);
+  }
+  fragment.appendChild(cube);
+  return fragment;
 }
