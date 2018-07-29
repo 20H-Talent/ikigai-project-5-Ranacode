b4e872908b0376eb6400ea06f0561d05be91ca3e
Ranacode <romerodeveloper@techie.com>
2018-07-29 09:47:36 +0100

    Change background functionality and make the image data structure on json format

--- js/easygallery.js
+++ js/easygallery.js
@@ -1,7 +1,6 @@
 const checkbox = document.querySelector("input[type=checkbox");
 
 checkbox.addEventListener("change", toggleSwitch, false);
-
 function toggleSwitch(e) {
   const checkbox = e.target;
   const label = checkbox.parentElement;
@@ -11,8 +10,10 @@ function toggleSwitch(e) {
   if (checkbox.checked) {
     sunIcon.style.color = "gray";
     moonIcon.style.color = "#055199";
+    document.body.style.backgroundColor = "#1e2019";
   } else {
     sunIcon.style.color = "#e4e40c";
     moonIcon.style.color = "gray";
+    document.body.style.backgroundColor = "#fafafa";
   }
 }
