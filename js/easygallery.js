80bbf1039c3a4c427ea788edbea40ce5046faaf0
Ranacode <romerodeveloper@techie.com>
2018-07-29 10:07:58 +0100

    Create folder named mockdata to simulate api request and fix layout on widths below 428px

--- js/easygallery.js
+++ js/easygallery.js
@@ -1,15 +1,42 @@
 const checkbox = document.querySelector("input[type=checkbox");
+let path = "/mockdata/images.json";
 
 checkbox.addEventListener("change", toggleSwitch, false);
+
+renderGalleryImages(path);
+
+function renderGalleryImages(path) {
+  const { host, protocol } = window.location;
+  const url = `${protocol}//${host}/${path}`;
+
+  fetch(url, {
+    method: "GET",
+    mode: "same-origin",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    referrer: "no-referrer"
+  })
+    .then(response => response.json())
+    .then(data => {
+      const images = data;
+    })
+    .catch(err => console.log(err));
+}
+
 function toggleSwitch(e) {
   const checkbox = e.target;
   const label = checkbox.parentElement;
+  changeBackgroundTheme(label, checkbox.checked);
+}
+
+function changeBackgroundTheme(label, checked) {
   const sunIcon = label.previousElementSibling;
   const moonIcon = label.nextElementSibling;
 
   if (checkbox.checked) {
     sunIcon.style.color = "gray";
-    moonIcon.style.color = "#055199";
+    moonIcon.style.color = "#fac9b8";
     document.body.style.backgroundColor = "#1e2019";
   } else {
     sunIcon.style.color = "#e4e40c";
